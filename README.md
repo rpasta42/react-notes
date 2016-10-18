87e098983dddbcc8ba07e6eecb414945d112cca1
Kostyantyn Kovalskyy <konstantin@icloak.org>
2016-10-18 05:59:00 +0000

    Dropdown toggle pains.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Add assets somehow.
 
 #https://startbootstrap.com/template-overviews/sb-admin-2/
+#http://www.pixeden.com/icon-fonts/stroke-7-icon-font-set
+
--- jsx/admin.jsx
+++ jsx/admin.jsx
@@ -44,21 +44,20 @@ class SidePanel extends React.Component {
 }
 
 
-class FaIcon { //<FaIcon name='blah' />
+class FaIcon extends React.Component { //<FaIcon name='blah' />
    render() {
       return (
-         <i className='fa fa-fw {this.props.name}'></i>
+         <i className={' ' + this.props.name}></i>
       );
    }
 }
 
-class MenuItemLink { //<MenuItemLink onclick='blah' sideImg=blah label='blah'>
+class MenuItemLink extends React.Component { //<MenuItemLink onclick='blah' sideImg=blah label='blah'>
    render() {
       return (
          <li>
-            <a href='#' onclick='{this.props.onclick}'>
-               { this.props.sideImg }
-               <p>{ this.props.label }</p>
+            <a href='#' onClick={this.props.onClick}>
+               <FaIcon name={ this.props.sideImg } /> { this.props.label }
             </a>
          </li>
       );
@@ -66,11 +65,71 @@ class MenuItemLink { //<MenuItemLink onclick='blah' sideImg=blah label='blah'>
 }
 
 
+class FoldingMenu extends React.Component {
+   constructor(props) {
+      super(props);
+      this.id = this.props.id;
+      this.sideImg = this.props.sideImg;
+      this.label = this.props.label;
+      this.subItems = this.props.subItems; //[{'key':'0', 'link':'https://blah', 'label':'blah'}]
+
+      this.kkClassName = ' ';
+      this.expanded = false;
+   }
+   componentDidMount() {
+      var e = this.expanded;
+      this.expanded = !e;
+   }
+
+   handleChange() {
+      console.log('bye');
+      if (this.kkClassName.length < 1) {
+         this.kkClassName = 'collapseWhite collapse';
+      }
+   }
+
+   render() {
+      console.log('hi');
+
+      return (
+         <li>
+            <a data-toggle='collapse' onClick={()=>(this.handleChange(this.kkClassName)} className={this.kkClassName} href={'#' + this.id} id={'#' + this.id + 'toggle'} aria-expanded={this.exnapded} aria-controls={'#' + this.id} >
+               <i className={this.sideImg}> </i>
+               <p>
+                  {this.label}
+                  <b className='caret'></b>
+               </p>
+            </a>
+            <div id={this.id} className='collapseWhite collapse' style={ {height:'5px'} }>
+               <ul className='nav'>
+                  {this.subItems.map(item => (
+                     <li key={item.key}><a href={item.link}>{item.label}</a></li>
+                  ))}
+               </ul>
+            </div>
+         </li>
+      );
+   }
+}
+
 class SidePanelNav extends React.Component {
    constructor(props) {
       super(props);
       this.topImgUrl = this.props.topImgUrl;
       this.topMenuImg = <img src={this.topImgUrl} />;
+
+
+      this.adminId = 'sidebar-admin-dropdown';
+      this.adminImg = 'pe-7s-gleam';
+      this.adminItems = [
+         { 'link':'blah', 'label':'hey'},
+         { 'link':'no', 'label':'way'}
+      ];
+
+      var i = 0;
+      for (var x in this.adminItems) {
+         this.adminItems[x]['key'] = i++;
+      }
    }
 
    render() {
@@ -79,27 +138,54 @@ class SidePanelNav extends React.Component {
       return (
          <BPanel body={sidepanelBody} id='kk-sidepanel'>
              {this.topMenuImg}
-         </BPanel>);
+         </BPanel>
+      );
    }
 
    getMenuItems() {
       this.items = [];
-      var sideImg = <FaIcon name='fa-dashboard' />;
-      this.items.push(<MenuItemLink onclick='alert("hi");' sideImg={sideImg} label='blah' />);
+      this.sideImg = 'fa-bar-chart-o fa fa-fw'; //<FaIcon name='fa-dashboard' />;
+      var i = 0;
+      this.items.push({
+         onclick : () => alert('bye'),
+         sideImg : this.sideImg,
+         label : 'blah',
+         key : i++
+      });
+      this.items.push({
+         onclick  : () => alert('hi'),
+         sideImg  :'fa-dashboard fa fa-fw',
+         label    : 'Dashboard',
+         key      : i++
+      });
+
+      this.items.push({
+         onclick  : () => alert('hi'),
+         sideImg  :'pe-7s-config',
+         label    : 'ACCOUNT SETTINGS',
+         key      : i++
+      });
+
+      this.items.push({
+         onclick  : () => alert('hi'),
+         sideImg  :'pe-7s-info',
+         label    : 'HELP',
+         key      : i++
+      });
+
 
       return (
          <div id='mypanel' className='navbar-default sidebar' role='navigation'>
             <div className='sidebar-nav navbar-collapse'>
                <ul className='nav in' id='side-menu'>
                   <li className='sidebar-search'>hi</li>
-                  {this.items.map(item => item)}
 
-                  <li>
-                     <a href='#'>
-                        <i className='fa fa-dashboard fa-fw'></i>
-                        Dashboard
-                     </a>
-                  </li>
+                  <FoldingMenu id={this.adminId} sideImg={this.adminImg} label='ADMIN' subItems={this.adminItems}>
+                  </FoldingMenu>
+
+                  {this.items.map(item => (
+                     <MenuItemLink key={item.key} onClick={item.onclick} sideImg={item.sideImg} label={item.label} />
+                  ))}
 
                   <li>
                      <a href='#'>
